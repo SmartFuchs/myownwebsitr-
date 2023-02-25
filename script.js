9e1de61aa8271922298d1b62bebbbb01486cd3b0
SmartFuchs <124798454+SmartFuchs@users.noreply.github.com>
2023-02-25 22:24:49 +0100

    My own website

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor() {
+  document.body.style.backgroundColor = "red";
+  document.body.style.color = "black";
+  
+}
+document.body.style.fontSize = "40px";
+document.querySelector("h1").style.fontSize = "100px";
+document.querySelector("h4").style.fontSize = "30px";

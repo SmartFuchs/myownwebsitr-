4320a8cb65d3c8761f2b73e64e885584f654addc
SmartVulpe <124798454+SmartFuchs@users.noreply.github.com>
2023-02-26 21:27:54 +0100

    Hi

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function changeColor() {
 document.body.style.fontSize = "40px";
 document.querySelector("h1").style.fontSize = "100px";
 document.querySelector("h4").style.fontSize = "30px";
+
+// Blinking effect
+setInterval(function() {
+	var element = document.getElementById("neon-text");
+	element.style.opacity = (element.style.opacity == 0 ? 1 : 0);
+}, 500);
